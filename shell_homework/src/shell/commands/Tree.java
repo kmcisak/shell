2d785d52b813e968ea8d558cfec40c478b86761d
kamil <km.cisak@gmail.com>
2016-04-26 11:14:38 +0200

    tree command implementation

--- shell_homework/src/shell/commands/Tree.java
+++ shell_homework/src/shell/commands/Tree.java
@@ -1,13 +1,30 @@
 package shell.commands;
 
+import java.io.File;
+
 import shell.commands.interfaces.Command;
 
 public class Tree implements Command {
 
 	@Override
 	public void execute() {
-		System.out.println("tree execute...");
+		File file = new File(".");
+		printDirectoryStructure(file, 1);
 
 	}
 
+	private void printDirectoryStructure(File dir, int depth) {
+
+		File files[] = dir.listFiles();
+		if (files != null) {
+			for (File file : files) {
+				if (file.isDirectory()) {
+					for (int i = 0; i < depth; i++) System.out.print("-");
+					System.out.println(file.getName());
+					printDirectoryStructure(file, ++depth);
+				}
+				--depth;
+			}
+		}
+	}
 }
