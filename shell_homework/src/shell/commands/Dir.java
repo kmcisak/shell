ba01fd1a1c42b3e75c135b1ff0e1422a2a335bff
kamil <km.cisak@gmail.com>
2016-04-26 10:04:51 +0200

    dir command implementation

--- shell_homework/src/shell/commands/Dir.java
+++ shell_homework/src/shell/commands/Dir.java
@@ -1,13 +1,28 @@
 package shell.commands;
 
+import java.io.File;
+
 import shell.commands.interfaces.Command;
 
-public class Dir implements Command{
+public class Dir implements Command {
 
 	@Override
 	public void execute() {
-		System.out.println("Dir execute...");
-		
+		File dir = new File(".");
+		printDir(dir);
+	}
+
+	private void printDir(File directory) {
+		File[] filesList = directory.listFiles();
+		for (File file : filesList) {
+			if (file.isDirectory()) {
+				System.out.println("DIR\t" + file.getName());
+			}
+			if (file.isFile()) {
+				System.out.println("FILE\t" + file.getName());
+
+			}
+		}
 	}
 
 }
