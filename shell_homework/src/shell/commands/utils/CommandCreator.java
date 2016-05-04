8d097614b472604a3fda6f9d0928e3da03de8535
kamil <km.cisak@gmail.com>
2016-05-04 16:47:25 +0200

    named exception

--- shell_homework/src/shell/commands/utils/CommandCreator.java
+++ shell_homework/src/shell/commands/utils/CommandCreator.java
@@ -3,29 +3,29 @@ package shell.commands.utils;
 import shell.commands.Cd;
 import shell.commands.Prompt;
 import shell.commands.interfaces.Command;
+import shell.exceptions.CommandNotSupportedException;
 
 public class CommandCreator {
-	
+
 	Prompt prompt;
 	CommandLine commandLine;
-	
+
 	public CommandCreator(Prompt prompt, CommandLine commandLine) {
 		this.commandLine = commandLine;
 		this.prompt = prompt;
 	}
 
-	public  Command createCommand() throws Exception {
+	public Command createCommand() throws Exception {
 		if (commandLine.getCommandName().equals("prompt")) {
 			prompt.setParameter(commandLine.getCommandParameter());
 			return prompt;
 		} else if (commandLine.getCommandName().equals("cd")) {
 			Cd cd = new Cd(prompt, commandLine);
-			
+
 			return cd.returnCd();
-			
-		} 
-		else {
-			throw new Exception(commandLine.getCommandName() + " : unknown command");
+
+		} else {
+			throw new CommandNotSupportedException(commandLine.getCommandName() + " : unknown command");
 		}
 	}
 
---
+++ shell_homework/src/shell/exceptions/CommandNotSupportedException.java
@@ -0,0 +1,10 @@
+package shell.exceptions;
+
+public class CommandNotSupportedException extends Exception{
+	
+	public CommandNotSupportedException(String message) {
+		super(message);
+	}
+	
+
+}
